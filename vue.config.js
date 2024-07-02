7c32a57d442cf2a270c5bd6a002e2c7c0dc1a20a
gk <iba3374@naver.com>
2024-07-02 20:54:00 +0900

    refactor: prettier & eslint 적용

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ module.exports = {
   devServer: {
     port: 3000,
     proxy: {
-      '/api' : {
-        target: 'http://localhost:8080'
-      }
-    }
-  }
-}
+      '/api': {
+        target: 'http://localhost:8080',
+      },
+    },
+  },
+};
