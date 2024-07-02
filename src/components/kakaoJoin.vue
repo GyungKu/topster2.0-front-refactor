89b8a04088acf2493731062c033a105772e0c0c9
gk <iba3374@naver.com>
2024-07-02 11:41:03 +0900

    refactor: eslint, prettier 적용 및 script setup으로 수정

--- src/components/kakaoJoin.vue
+++ src/components/kakaoJoin.vue
@@ -1,39 +1,33 @@
-<script>
+<script setup>
+import axios from 'axios';
+import { useRouter, useRoute } from 'vue-router';
+import useStore from 'vuex';
 
-import axios from "axios";
-import router from "@/scripts/router";
+const route = useRoute();
+const router = useRouter();
+const store = useStore();
+const code = ref('');
 
-export default {
-
-  data() {
-    return {
-      code: null,
-    }
-  },
-
-  created() {
-    this.code = this.$route.query.code;
-    axios.get(`/users/kakao/callback?code=` + this.code)
+onMounted(() => {
+  code.value = route.query.code;
+  axios
+    .get(`/users/kakao/callback?code=${code.value}`)
     .then((res) => {
-      const access = res.headers['authorization'];
-      const refresh = res.headers['refreshtoken'];
-      axios.defaults.headers.common['authorization'] = access;
-      this.$store.dispatch('setToken', access);
+      const access = res.headers.authorization;
+      const refresh = res.headers.refreshtoken;
+      axios.defaults.headers.common.authorization = access;
+      store.dispatch('setToken', access);
       const accessToken = {
         token: access,
-        expire: Date.now() + (58 * 60 * 1000),
-      }
+        expire: Date.now() + 58 * 60 * 1000,
+      };
       const refreshToken = {
         token: refresh,
-        expire: Date.now() + (60 * 60 * 1000 * 24 * 7) - 6000
-      }
-      localStorage.setItem("accessToken", JSON.stringify(accessToken));
-      localStorage.setItem("refreshToken", JSON.stringify(refreshToken));
-    }).then(() => router.push('/'))
-  },
-
-
-
-}
-
+        expire: Date.now() + 60 * 60 * 1000 * 24 * 7 - 6000,
+      };
+      localStorage.setItem('accessToken', JSON.stringify(accessToken));
+      localStorage.setItem('refreshToken', JSON.stringify(refreshToken));
+    })
+    .then(() => router.push('/'));
+});
 </script>
