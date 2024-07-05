ef917231395945e6e132352547c270e327c03bca
gk <iba3374@naver.com>
2024-07-05 14:52:34 +0900

    fix: comment form과 list 합치고 comment 관련 함수 commentlist로 이동

--- src/components/CommentList.vue
+++ src/components/CommentList.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="comment-list">
     <ul>
-      <li
-        v-for="comment in props.comments"
-        :key="comment.id"
-        class="comment-item"
-      >
-        <div v-if="!comment.isEditing">
+      <li v-for="comment in comments" :key="comment.id" class="comment-item">
+        <div v-if="!isEditing(comment.id)">
           {{ comment.content }} - 작성자: {{ comment.author }} - 작성일:
           {{ formatDate(comment.createdAt) }}
         </div>
-        <div v-if="comment.isEditing" class="editing-comment">
+        <div v-if="isEditing(comment.id)" class="editing-comment">
           <textarea v-model="comment.content" class="form-control"></textarea>
           <div class="comment-buttons">
             <button
@@ -24,16 +20,17 @@
             </button>
           </div>
         </div>
-        <div v-if="comment.isEditing" class="comment-buttons">
+        <div
+          v-if="store.state.token && !isEditing(comment.id)"
+          class="comment-buttons"
+        >
           <button
-            v-if="store.state.token"
-            @click="toggleEdit(comment)"
+            @click="toggleEdit(comment.id)"
             class="btn btn-primary btn-sm"
           >
             수정
           </button>
           <button
-            v-if="store.state.token"
             @click="deleteComment(comment.id)"
             class="btn btn-danger btn-sm"
           >
@@ -43,17 +40,118 @@
       </li>
     </ul>
   </div>
+
+  <div class="pagination">
+    <button
+      @click="prevPage"
+      :disabled="currentPage <= 1"
+      class="pagination-button"
+    >
+      이전
+    </button>
+    <span class="pagination-current-page">{{ currentPage }}</span>
+    <button
+      @click="nextPage"
+      :disabled="currentPage >= lastPage"
+      class="pagination-button"
+    >
+      다음
+    </button>
+  </div>
+
+  <form @submit.prevent="submitComment" class="comment-form">
+    <label for="newComment" class="form-label">댓글 작성:</label>
+    <div class="form-input-container">
+      <textarea
+        v-model="newComment"
+        id="newComment"
+        class="form-textarea"
+        required
+      ></textarea>
+      <button type="submit" class="form-button">댓글 작성</button>
+    </div>
+  </form>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import axios from 'axios';
 import { useStore } from 'vuex';
+import { useRoute, useRouter } from 'vue-router';
 
-const props = defineProps({ comments: [] });
-const emit = defineEmits(['editComment', 'deleteComment']);
+const props = defineProps({ postId: Number });
+const router = useRouter();
+const route = useRoute();
 const store = useStore();
-const isEditing = ref(false);
+const currentPage = ref(1);
+const lastPage = ref(0);
+const comments = ref([]);
+const editingCommentIds = ref([]);
+const newComment = ref('');
+
+const loadComments = () => {
+  // 댓글 목록을 다시 불러오는 API 호출
+  axios
+    .get(`/posts/${props.postId}/comments`, {
+      params: {
+        page: currentPage.value,
+      },
+    })
+    .then((response) => {
+      comments.value = response.data.content;
+      lastPage.value = response.data.totalPages;
+    })
+    .catch((error) => {
+      console.error('댓글 목록을 불러오는 중 오류 발생:', error);
+    });
+};
+
+const submitComment = () => {
+  axios
+    .post(`/posts/${props.postId}/comments`, {
+      content: newComment.value,
+    })
+    .then((res) => {
+      if (res.data.code === '1009') {
+        alert('로그인을 먼저 해주세요');
+        const redirect = route.path;
+        router.push({ name: 'login', query: { redirect } });
+      }
+      newComment.value = '';
+      loadComments();
+    })
+    .catch((error) => {
+      console.error('댓글 작성 중 오류 발생:', error);
+    });
+};
+
+const editComment = async (comment) => {
+  const dataForm = {
+    content: comment.content,
+  };
+  await axios
+    .patch(`/comments/${comment.id}`, dataForm)
+    .then(() => {
+      alert('수정 성공');
+      cancelEdit(comment.id);
+      loadComments();
+    })
+    .catch(() => {
+      alert('무언가 오류가 발생');
+    });
+};
+
+const deleteComment = (commentId) => {
+  axios
+    .delete(`/comments/${commentId}`)
+    .then(() => {
+      alert('삭제 완료');
+      loadComments();
+    })
+    .catch(() => {
+      alert('본인의 댓글이 아닙니다.');
+    });
+};
 
 const padZero = (value) => {
   return value < 10 ? `0${value}` : value;
@@ -65,29 +163,58 @@ const formatDate = (createdAt) => {
   return formattedDate;
 };
 
-const toggleEdit = (comment) => {
+const isEditing = (commentId) => {
+  return editingCommentIds.value.includes(commentId);
+};
+
+const toggleEdit = (commentId) => {
   axios
-    .get(`/comments/${comment.id}/isAuthor`)
+    .get(`/comments/${commentId}/isAuthor`)
     .then(() => {
-      isEditing.value = true;
+      editingCommentIds.value.push(commentId);
     })
     .catch(() => {
-      isEditing.value = false;
       alert('본인의 댓글이 아닙니다.');
     });
 };
 
-const cancelEdit = () => {
-  isEditing.value = false;
+const cancelEdit = (commentId) => {
+  editingCommentIds.value = editingCommentIds.value.filter(
+    (id) => id !== commentId,
+  );
 };
 
-const editComment = (comment) => {
-  emit('editComment', comment);
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value -= 1;
+    loadComments();
+  }
 };
 
-const deleteComment = (id) => {
-  emit('deleteComment', id);
+const nextPage = () => {
+  if (currentPage.value < lastPage.value) {
+    // 현재 페이지가 마지막 페이지보다 작은 경우에만 다음 페이지로 이동
+    currentPage.value += 1;
+    loadComments();
+  }
 };
+
+watch(
+  () => props.postId,
+  (postId) => {
+    if (postId) {
+      loadComments();
+      console.log(`watch = ${postId}`);
+    }
+  },
+);
+
+onMounted(() => {
+  if (props.postId) {
+    loadComments();
+    console.log(props.postId);
+  }
+});
 </script>
 
 <style scoped>
@@ -123,5 +250,69 @@ const deleteComment = (id) => {
 .btn-sm {
   padding: 5px 10px;
   font-size: 12px;
+}
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.pagination-button {
+  padding: 10px;
+  margin: 0 5px;
+  border: none;
+  background-color: #f0f0f0;
+  cursor: pointer;
+}
+
+.pagination-button:disabled {
+  background-color: #ddd;
+  cursor: not-allowed;
+}
+
+.pagination-current-page {
+  padding: 10px;
+}
+
+.comment-form {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form-label {
+  margin-bottom: 5px;
+  display: block;
+  font-weight: bold;
+}
+
+.form-input-container {
+  display: flex;
+  align-items: center;
+  width: 50%; /* 폼 전체 폭을 사용 */
+}
+
+.form-textarea {
+  margin-bottom: 15px;
+  padding: 10px;
+  font-size: 16px;
+  flex: 1; /* 입력칸이 남은 공간을 모두 차지하도록 */
+  box-sizing: border-box; /* padding, border를 포함한 전체 크기로 설정 */
+}
+
+.form-button {
+  background-color: #4caf50;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-left: 10px; /* 입력칸과의 간격 조절 */
+}
+
+.form-button:hover {
+  background-color: #45a049;
 }
 </style>
--- src/pages/PostDetail.vue
+++ src/pages/PostDetail.vue
@@ -5,34 +5,14 @@
     <PostContent :post="post" />
 
     <CommentList
-      :comments="comments"
+      :postId="post.id"
       @delete-comment="deleteComment"
       @edit-comment="editComment"
     />
-    <div class="pagination">
-      <button
-        @click="prevPage"
-        :disabled="currentPage <= 1"
-        class="pagination-button"
-      >
-        이전
-      </button>
-      <span class="pagination-current-page">{{ currentPage }}</span>
-      <button
-        @click="nextPage"
-        :disabled="currentPage >= lastPage"
-        class="pagination-button"
-      >
-        다음
-      </button>
-    </div>
-
-    <CommentForm @submit-comment="submitComment" />
   </div>
 </template>
 
 <script setup>
-import CommentForm from '@/components/CommentForm.vue';
 import CommentList from '@/components/CommentList.vue';
 import PostContent from '@/components/PostContent.vue';
 import TopsterCard from '@/components/TopsterCard.vue';
@@ -43,90 +23,7 @@ import { useRoute, useRouter } from 'vue-router';
 const route = useRoute();
 const router = useRouter();
 const post = ref({});
-const comments = ref([]);
 const topster = ref({});
-const currentPage = ref(1);
-const lastPage = ref(0);
-
-const submitComment = (newComment) => {
-  const { postId } = route.params;
-  axios
-    .post(`/posts/${postId}/comments`, {
-      content: newComment,
-    })
-    .then((res) => {
-      if (res.data.code === '1009') {
-        alert('로그인을 먼저 해주세요');
-        const redirect = route.path;
-        router.push({ name: 'login', query: { redirect } });
-      }
-      // 댓글 목록 다시 불러오기
-      this.loadComments();
-    })
-    .catch((error) => {
-      console.error('댓글 작성 중 오류 발생:', error);
-    });
-};
-
-const loadComments = () => {
-  // 댓글 목록을 다시 불러오는 API 호출
-  const { postId } = route.params.id;
-  axios
-    .get(`/posts/${postId}/comments`, {
-      params: {
-        page: currentPage.value,
-      },
-    })
-    .then((response) => {
-      comments.value = response.data.content;
-      lastPage.value = response.data.totalPages;
-    })
-    .catch((error) => {
-      console.error('댓글 목록을 불러오는 중 오류 발생:', error);
-    });
-};
-
-const editComment = (comment) => {
-  const dataForm = {
-    content: comment.content,
-  };
-  axios
-    .patch(`/comments/${comment.id}`, dataForm)
-    .then(() => {
-      alert('수정 성공');
-      this.loadComments();
-    })
-    .catch(() => {
-      alert('무언가 오류가 발생');
-    });
-};
-
-const deleteComment = (commentId) => {
-  axios
-    .delete(`/comments/${commentId}`)
-    .then(() => {
-      alert('삭제 완료');
-      this.loadComments();
-    })
-    .catch(() => {
-      alert('본인의 댓글이 아닙니다.');
-    });
-};
-
-const prevPage = () => {
-  if (currentPage.value > 1) {
-    currentPage.value -= 1;
-    loadComments();
-  }
-};
-
-const nextPage = () => {
-  if (currentPage.value < lastPage.value) {
-    // 현재 페이지가 마지막 페이지보다 작은 경우에만 다음 페이지로 이동
-    currentPage.value += 1;
-    this.loadComments();
-  }
-};
 
 onMounted(() => {
   // 게시글 정보와 댓글 목록을 불러오는 API 호출
@@ -145,41 +42,5 @@ onMounted(() => {
       router.push('/posts');
       alert('존재하지 않는 게시글입니다.');
     });
-
-  axios
-    .get(`/posts/${postId}/comments`)
-    .then((response) => {
-      comments.value = response.data.content;
-      lastPage.value = response.data.totalPages;
-    })
-    .catch(() => {
-      alert('댓글 목록을 불러오는 중 오류 발생:');
-    });
 });
 </script>
-
-<style scoped>
-.pagination {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 20px;
-}
-
-.pagination-button {
-  padding: 10px;
-  margin: 0 5px;
-  border: none;
-  background-color: #f0f0f0;
-  cursor: pointer;
-}
-
-.pagination-button:disabled {
-  background-color: #ddd;
-  cursor: not-allowed;
-}
-
-.pagination-current-page {
-  padding: 10px;
-}
-</style>
