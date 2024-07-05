dfe076af6aa4a77ed356052985ee8934902dd448
gk <iba3374@naver.com>
2024-07-05 11:41:58 +0900

    refactor: store.js에 eslint & prettier 적용

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -1,41 +1,38 @@
-import {createStore} from 'vuex'
-import axios from "axios";
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 // Create a new store instance.
 const store = createStore({
   state() {
-    const accessToken = JSON.parse(localStorage.getItem("accessToken"));
+    const accessToken = JSON.parse(localStorage.getItem('accessToken'));
     // access token이 존재하고, 유효기간이 남았다면
     if (accessToken && Date.now() < accessToken.expire) {
       return {
-        token: accessToken.token
+        token: accessToken.token,
       };
-    } else {
-      return {
-        token: null
-      }}
-    },
+    }
+    return {
+      token: null,
+    };
+  },
 
   mutations: {
     setToken(state, accessToken) {
       state.token = accessToken;
-      return {state};
     },
   },
   actions: {
-
-    setToken({commit}, accessToken) {
+    setToken({ commit }, accessToken) {
       commit('setToken', accessToken);
     },
 
-    logout({commit}) {
+    logout({ commit }) {
       alert('로그아웃 성공');
-      localStorage.removeItem("accessToken");
-      localStorage.removeItem("refreshToken");
-      commit("setToken", null);
-      axios.defaults.headers.common['Authorization'] = null;
-    }
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('refreshToken');
+      commit('setToken', null);
+      axios.defaults.headers.common.Authorization = null;
+    },
   },
-
-})
+});
 export default store;
